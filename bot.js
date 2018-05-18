41587d7b1a6836164062300061f4545cfac9c565
Mikko Myllys <mikko.myllys@eng.tamk.fi>
2018-05-18 21:00:43 +0300

    WIP

--- bot.js
+++ bot.js
@@ -5,10 +5,19 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', async message => {
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+	// and not get into a spam loop (we call that "botception").
+	if(message.author.bot) return;
+	
+	
+	// Also good practice to ignore any message that does not start with our prefix, 
+	// which is set in the configuration file.
+	if(message.content.indexOf("!") !== 0) return;
+	
+	if(message.content == "JanneLeipoo"){
+		message.channel.send("Mitä se janne leipoo?");
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
